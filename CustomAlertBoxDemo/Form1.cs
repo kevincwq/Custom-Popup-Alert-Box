27a05add8d6a59b192ad0088312970f43e605af5
Peter Chen <kevincwq@gmail.com>
2020-06-05 12:14:29 -0600

    Add project files.

---
+++ CustomAlertBoxDemo/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CustomAlertBoxDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Alert(string title, string msg, Form_Alert.enmType type)
+        {
+            Form_Alert frm = new Form_Alert();
+            frm.showAlert(title, msg, type);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Alert("Success Alert", "Request is conducted successfully", Form_Alert.enmType.Success);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Alert("Warning Alert", "This is a long message: there is a warning during downloading file from cloud server", Form_Alert.enmType.Warning);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Alert("Error Alert", "Request failed because there is no internet connection", Form_Alert.enmType.Error);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Alert("Info Alert", "The application will try again to download the license in 5 minutes", Form_Alert.enmType.Info);
+        }
+    }
+}
---
+++ CustomAlertBoxDemo/Form_Alert.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CustomAlertBoxDemo
+{
+    partial class Form_Alert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblMsg = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoEllipsis = true;
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(56, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(281, 29);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CustomAlertBoxDemo.Properties.Resources.success;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CustomAlertBoxDemo.Properties.Resources.icons8_cancel_25px;
+            this.pictureBox2.Location = new System.Drawing.Point(353, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(26, 30);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoEllipsis = true;
+            this.lblMsg.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.ForeColor = System.Drawing.Color.White;
+            this.lblMsg.Location = new System.Drawing.Point(56, 38);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(281, 43);
+            this.lblMsg.TabIndex = 4;
+            this.lblMsg.Text = "Text";
+            // 
+            // Form_Alert
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(385, 91);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_Alert";
+            this.Text = "Form_Alert";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        internal System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
